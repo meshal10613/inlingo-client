bafbaebf2fd4858efa75bd65381ad3c1a4108853
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-08-09 21:12:24 +0600

    banner section updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
     const { user } = useAuthContext();
     return (
         <div className='flex flex-col-reverse md:flex-row items-center justify-between my-12 md:my-20'>
-            <div className='space-y-4'>
+            <div className='space-y-10 text-center md:text-left'>
                 <h2 className='text-7xl font-bold'>Learn faster with your best language tutor.</h2>
                 <Link to={`${user ? "/findTutors" : "/login"}`} className='btn text-[16px] border-1 border-[#7F3D27] text-[#D9D9D9] bg-[#7F3D27] transition-all hover:text-[#7F3D27] hover:bg-[#D9D9D9]'>Get Started <FaArrowRight /></Link>
             </div>
